8e26f011329d8d609716eb12e1893f114d0d7a73
Tom Fischer <tgfischer6@gmail.com>
2017-01-05 11:30:39 -0500

    Can now accept signup requests, and issue a json web token

---
+++ src/nls/messages.js
@@ -0,0 +1,5 @@
+export const nls = {
+  GENERIC_ERROR_MESSAGE: 'Something has gone wrong while processing your request. Please try again later.',
+  INVALID_LOGIN_CREDENTIALS: 'Oops! Either the email or password that was provided is incorrect.',
+  USER_ALREADY_EXISTS: 'Oops! There is already an account that uses that email address.'
+};
--- src/routes/login.js
+++ src/routes/login.js
@@ -1,21 +1,61 @@
 import express from 'express';
 import jwt from 'jsonwebtoken';
+import bcrypt from 'bcrypt-nodejs';
 import {pool} from '../app';
+import {nls} from '../nls/messages';
+
 const router = express.Router();
 
 router.post('/', (req, res) => {
-  console.log(JSON.stringify(req.body, null, 2));
-
-  const user = {
+  const creds = {
     email: req.body.email,
     password: req.body.password
   };
 
-  user.token = jwt.sign(user, process.env.JWT_SECRET);
+  pool.connect().then(client => {
+    client.query(`SELECT email, password FROM users WHERE email='${creds.email}' LIMIT 1`).then(result => {
+      client.release();
 
-  res.status(200).json({
-    ok:true,
-    user
+      let user = result.rows[0];
+
+      if (user) {
+        // Check to see if the hashed password matches the one in the database
+        const hashMatches = bcrypt.compareSync(creds.password, user.password);
+
+        // Check that the correct user was returned from the database
+        if (hashMatches && user.email === creds.email) {
+          // Delete the password field, since we don't want to return that to the
+          // client
+          delete user.password;
+
+          // Generate the token for the user
+          user.token = jwt.sign(creds, process.env.JWT_SECRET);
+
+          // Output the log in credentials for debugging purposes
+          console.log('LOGIN: ' + JSON.stringify(user, null, 2));
+
+          // Return the user that was fetched from the database
+          res.status(200).json({user});
+        } else {
+          // Wait 3 seconds before returning the error
+          setTimeout(() => {
+            res.status(500).json({err: nls.INVALID_LOGIN_CREDENTIALS});
+          }, 3000);
+        }
+      } else {
+        // Wait 3 seconds before returning the error
+        setTimeout(() => {
+          res.status(500).json({err: nls.INVALID_LOGIN_CREDENTIALS});
+        }, 3000);
+      }
+    }).catch(err => {
+      client.release();
+      console.error('ERROR: ', err.message, err.stack);
+
+      res.status(500).json({
+        err: err
+      });
+    });
   });
 });
 
