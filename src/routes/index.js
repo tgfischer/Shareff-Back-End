c791d8f613aed1c467ab19d2f64a66cede5ead19
Tom Fischer <tgfischer6@gmail.com>
2017-01-09 19:38:39 -0500

    Added functions for getting the user object from the token

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,8 +1,20 @@
 import express from 'express';
+import {getPayload} from '../utils/Utils';
 const router = express.Router();
 
 router.get('/', (req, res) => {
   res.status(200).send('Shareff');
+});
+
+router.post('/get_user', (req, res) => {
+  console.log(`\n${req.body.token}`);
+  getPayload(req.body.token).then(payload => {
+    delete payload.iat;
+    
+    return res.status(200).json({user: payload});
+  }).catch(err => {
+    return res.status(401).json({err});
+  });
 });
 
 export {router as index}
--- src/utils/Utils.js
+++ src/utils/Utils.js
@@ -1,31 +1,32 @@
 import jwt from 'jsonwebtoken';
 
 /**
- * This function is used to verify that the JWT that was supplied by the client
- * is valid, so the user can access restricted routes.
+ * This function retrieves the payload from the JWT (user information). It can
+ * also be used to verify that the JWT that was supplied by the client is valid,
+ * so the user can access restricted routes.
  *
  * Returns a promise, so you can do:
  *
- *    import {Utils} from '../utils/Utils';
+ *    import Utils from '../utils/Utils';
  *
- *    Utils.isAuthenticated(token).then((token) => {
+ *    Utils.getPayload(token).then(payload => {
  *      // Do something
- *    }).catch((err) => {
+ *    }).catch(err => {
  *      // Set the status to 401 Unauthorized, and return the error
  *      return res.status(401).json({err});
  *    });
  */
-export const isAuthenticated = (token) => {
+export const getPayload = (token) => {
   // Make a new promise
   return new Promise((resolve, reject) => {
     // Verify that the token is valid using the JWT secret that was used to sign
     // the user when they logged in
-    jwt.verify(token, process.env.JWT_SECRET, (err, token) => {
+    jwt.verify(token, process.env.JWT_SECRET, (err, payload) => {
       if (err) {
         return reject(err);
       }
 
-      return resolve(token);
+      return resolve(payload);
     });
   });
 };
