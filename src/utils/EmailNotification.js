0ec5d9ff23fb8de5196c683e65b6c882c210c105
agencare89 <agencare@uwo.ca>
2017-01-25 10:57:30 -0500

    Adding new files for tracking/rent request work

---
+++ src/utils/EmailNotification.js
@@ -0,0 +1,27 @@
+import {nls} from '../i18n/en';
+
+/**
+ * The following utility methods will be used to send specific types of 
+ * email notifications to users. 
+ * 
+ * Email notifications may include: 
+ *      - someone wants to rent your item! 
+ *  Upcoming reminders
+ *      - your booking is coming up!
+ *      - your item is being rented soon! 
+ *      - your rented item is due back soon!
+ *      - your item is being returned soon!
+ *  Confirmation of items
+ *      - did you receive your rental item?
+ *      - did you deliver your rental item?
+ *      - did you return your rental item? 
+ *      - was your rental item returned? 
+ */
+
+/**
+ * 
+ */
+export const rentalItemRequest = (blah) => {
+
+}
+
