2500bc853d20db0c78b5c28b146f9ec6fc58ed07
Tom Fischer <tgfischer6@gmail.com>
2017-02-04 11:28:39 -0500

    Merging

--- src/routes/profile/messages.js
+++ src/routes/profile/messages.js
@@ -3,8 +3,44 @@ import {pool} from '../../app';
 import {isLoggedIn} from '../../utils/Utils';
 const router = express.Router();
 
-router.post('/get_messages', isLoggedIn, (req, res) => {
-  const {userId, recipient} = req.body;
+router.post('/get_conversations', isLoggedIn, (req, res) => {
+  const {userId} = req.body;
+
+  console.log(userId);
+
+  // Connect to the pool, and grab a client
+  pool.connect().then(client => {
+    const query = 'SELECT "conversation"."id", "conversation"."rentRequestId", "conversation"."startDate", ( \
+                      SELECT row_to_json(ROW(row)) \
+                      FROM ( \
+                        SELECT "userTable".* FROM "conversation" INNER JOIN "userTable" ON "conversation"."ownerId"="userTable"."userId" \
+                      ) row \
+                    ) AS "owner", ( \
+                      SELECT row_to_json(ROW(row)) \
+                      FROM ( \
+                        SELECT "userTable".* FROM "conversation" INNER JOIN "userTable" ON "conversation"."renterId"="userTable"."userId" \
+                      ) row \
+                    ) AS "renter" \
+                  FROM "conversation" \
+                  WHERE "conversation"."renterId"=$1 OR "conversation"."ownerId"=$1';
+
+    client.query(query, [userId]).then(result => {
+      client.release();
+      const conversations = result.rows;
+
+      console.log(JSON.stringify(conversations, null, 2));
+
+      res.status(200).json({conversations});
+    }).catch(err => {
+      client.release();
+
+      console.error('ERROR: ', err.message, err.stack);
+      res.status(500).json({err});
+    });
+  }).catch(err => {
+    console.error('ERROR: ', err.message, err.stack);
+    res.status(500).json({err});
+  });
 });
 
 export {router as messages}
--- src/routes/profile/myItems.js
+++ src/routes/profile/myItems.js
@@ -12,14 +12,16 @@ router.post('/my_items', (req, res) => {
 
   // Connect to the pool, and grab a client
   pool.connect().then(client => {
-    const query = `SELECT "title", "category", "description", "price", "termsOfUse" FROM "rentalItem" WHERE "ownerId"=$1`;
+    const query = 'SELECT "title", "category", "description", "price", "termsOfUse" FROM "rentalItem" WHERE "ownerId"=$1';
+
     client.query(query, [ownerId]).then(result => {
       client.release();
-      const myItems = result.rows;
 
+      const myItems = result.rows;
       res.status(200).json({myItems});
     }).catch(err => {
       client.release();
+
       console.error('ERROR: ', err.message, err.stack);
       res.status(500).json({err});
     });
