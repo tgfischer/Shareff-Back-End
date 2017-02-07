9e257cee75f57b0bc4d5eb09248c9f4c41bef1a9
David <dnisenbaum1@gmail.com>
2017-02-07 18:22:47 -0500

    fixed up input field

--- src/routes/profile/addItem.js
+++ src/routes/profile/addItem.js
@@ -7,10 +7,10 @@ const router = express.Router();
 /**
  * Upload a rental item
  */
-router.post('/upload_item', isLoggedIn, (req, res) => {
+router.post('/add_item', isLoggedIn, (req, res) => {
   // Get the item details from the request
   const {
-    title, category, description, price, addressId, terms, userId
+    title, category, description, price, addressId, terms, userId, costPeriod, photos
   } = req.body;
 
   // Connect to the pool, and grab a client
@@ -18,7 +18,7 @@ router.post('/upload_item', isLoggedIn, (req, res) => {
     const query = `INSERT INTO public."rentalItem" ("title", "category", "description", "price", "addressId", "termsOfUse", "ownerId", "costPeriod", "photo") \
       VALUES ($1, ARRAY[$2], $3, $4, $5, $6, $7, $8, ARRAY[$9]);`;
 
-    client.query(query, [title, category, description, price, addressId, terms, userId, "days", "temp.jpg"]).then(result => {
+    client.query(query, [title, category, description, price, addressId, terms, userId, costPeriod, photos]).then(result => {
       client.release();
       res.status(200).json({success: true});
     }).catch(err => {
@@ -32,4 +32,4 @@ router.post('/upload_item', isLoggedIn, (req, res) => {
   });
 });
 
-export {router as uploadItem}
+export {router as addItem}
