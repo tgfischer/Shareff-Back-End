3e3bb81c607f9ae648c914132fe13d418ad14273
David <dnisenbaum1@gmail.com>
2017-02-25 19:57:42 -0500

    fixed client.release() error

--- src/routes/profile/billing.js
+++ src/routes/profile/billing.js
@@ -9,39 +9,35 @@ const router = express.Router();
  */
 router.post('/update_billing_info', isLoggedIn, (req, res) => {
   // Get the cc details from the request
-  const {expiryDate, userId, token} = req.body;
+  const {expiryDate, userId, stripeCustomerId, token} = req.body;
 
   // Connect to the pool, and grab a client
   pool.connect().then(client => {
-    getUser(client, userId, token).then(user => {
-      stripe.customers.retrieve(user.stripeCustomerId).then(customer => {
-        stripe.customers.updateCard(
-          customer.id,
-          customer.default_source,
-          {
-            exp_month: expiryDate.month,
-            exp_year: expiryDate.year
-          }).then(card => {
-            const query = `UPDATE public."userTable" SET "ccExpiryDate" = $1 WHERE "userId" = $2`;
+    stripe.customers.retrieve(stripeCustomerId).then(customer => {
+      stripe.customers.updateCard(
+        customer.id,
+        customer.default_source,
+        {
+          exp_month: expiryDate.month,
+          exp_year: expiryDate.year
+        }
+      ).then(card => {
+        const query = `UPDATE public."userTable" SET "ccExpiryDate" = $1 WHERE "userId" = $2`;
 
-            client.query(query, [expiryDate, userId]).then(result => {
-              res.status(200).json({user});
-            }).catch(err => {
-              console.log(err);
-              res.status(500).json({err});
-            });
+        client.query(query, [expiryDate, userId]).then(result => {
+          // need to send back updated user
+          getUser(client, userId, token).then(user => {
+            res.status(200).json({user});
+          }).catch(err => {
+            res.status(500).json({err});
+          });
         }).catch(err => {
-          // failed to update card error
-          console.log(err);
           res.status(500).json({err});
         });
       }).catch(err => {
-        // failed to retrieve customer from stripe error
-        console.log(err);
         res.status(500).json({err});
       });
     }).catch(err => {
-      // failed to get user error
       res.status(500).json({err});
     });
   }).catch(err => {
