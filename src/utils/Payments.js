57cd02833f0a60c05248cb5b2f6d3d4e2d8a8c34
David <dnisenbaum1@gmail.com>
2017-03-08 12:44:00 -0500

    fixed query

--- src/utils/Payments.js
+++ src/utils/Payments.js
@@ -2,8 +2,8 @@ import {stripe, convertDate} from './Utils';
 import {pool} from '../app';
 import {nls} from '../i18n/en';
 
-export const completeTransaction = (renterId, ownerId, amount) => {
-  const paymentQuery = `UPDATE "booking" SET "paymentStatus" = $1`;
+export const completeTransaction = (renterId, ownerId, amount, bookingId) => {
+  const paymentQuery = `UPDATE "booking" SET "paymentStatus" = $1 WHERE "bookingId" = $2`;
 
   pool.connect().then(client => {
     const getCustomerQuery = `SELECT "stripeCustomerId" FROM "userTable" WHERE "userId" = $1`;
@@ -29,21 +29,21 @@ export const completeTransaction = (renterId, ownerId, amount) => {
             }
           }).then(result => {
             console.log("payment completed successfully");
-            client.query(paymentQuery, [nls.PAYMENT_COMPLETED]).then(result => {
+            client.query(paymentQuery, [nls.PAYMENT_COMPLETED, bookingId]).then(result => {
               client.release();
             }).catch(err => {
               console.log(err);
             });
           }).catch(err => {
             console.log(err);
-            client.query(paymentQuery, [nls.PAYMENT_FAILED]).then(result => {
+            client.query(paymentQuery, [nls.PAYMENT_FAILED, bookingId]).then(result => {
               client.release();
             }).catch(err => {
               console.log(err);
             });
           });
         }).catch(err => {
-          client.query(paymentQuery, [nls.PAYMENT_FAILED]).then(result => {
+          client.query(paymentQuery, [nls.PAYMENT_FAILED, bookingId]).then(result => {
             client.release();
           }).catch(err => {
             console.log(err);
@@ -51,7 +51,7 @@ export const completeTransaction = (renterId, ownerId, amount) => {
           console.log(err);
         });
       }).catch(err => {
-        client.query(paymentQuery, [nls.PAYMENT_FAILED]).then(result => {
+        client.query(paymentQuery, [nls.PAYMENT_FAILED, bookingId]).then(result => {
           client.release();
         }).catch(err => {
           console.log(err);
@@ -59,7 +59,7 @@ export const completeTransaction = (renterId, ownerId, amount) => {
         console.log(err);
       });
     }).catch(err => {
-      client.query(paymentQuery, [nls.PAYMENT_FAILED]).then(result => {
+      client.query(paymentQuery, [nls.PAYMENT_FAILED, bookingId]).then(result => {
         client.release();
       }).catch(err => {
         console.log(err);
